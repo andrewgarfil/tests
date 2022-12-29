3286d6b32a6b1c1ccf6157f1f9c7a54c147e0b31
andrewgarfil <120245361+andrewgarfil@users.noreply.github.com>
2022-12-29 19:38:41 +0400

    Add files via upload

---
+++ juk.js
@@ -0,0 +1,88 @@
+
+ТЕСТ 1
+
+1)
+
+2) let a = 4
+   let b = 5
+   console.log(a + b)
+
+3)$ и _
+
+4)let a;
+  let 3abc = 45; неправильная
+  let nre4 = 35;
+
+5)let answer = 0;
+  let alert = 45; неправильная
+  let errorMessage = 456;
+
+6)не ускоренный код
+  let a = 10;
+  a = a + 5;
+  console.log(a); 15
+  ускоренный код
+  let a = 10;
+  a += 5;
+  console.log(a);
+
+7)что выведет переменная let a;
+  null
+  ничего
+  undefined
+  выведет undefined
+
+8)укоротите скрипт ниже
+  let a = 6;
+  let b = 7;
+  let c = 'Alex';
+  let d = [6,7,'alex']
+
+9)что выведет консоль
+  let a = 6;
+  {
+    let a = 7;
+  }
+  console.log(a);
+  выведет 6
+
+10)что выведет консоль
+   let a = 6;
+   {
+    let a = 7;
+    console.log(a);
+   }
+   выведет 7
+
+11)что выведет консоль
+   let a = 6;
+  {
+    console.log(a);
+  }
+  выведет undefined
+
+12)что выведет консоль
+   const a = 6;
+   a = 7;
+   console.log(a);
+   константу нельзя изменить
+
+13)?
+
+14)console.log(2**4);
+
+15)console.log(5%4);
+
+16)укажите знак равно
+  =
+  ==
+  ===
+  равно это ==
+
+17)укажите строгое равно
+   =
+   ==
+   ===
+
+  
+								
